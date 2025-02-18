6d148233d37554e8857be7707aadfcce4d327089
Marcos Santos <markeka.mar@gmail.com>
2025-02-17 20:30:40 -0400

    adicao de metodos add nomes na lista

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-// Vamos iniciar a brincadeira na próxima quarta!!! Consultar bastante o jogo do número secreto...
+let addAmigo = document.getElementById("amigo");
+let amigos = [];
+document.getElementById("amigo").focus();
+
+function adicionarAmigo() {
+  amigos.push(addAmigo.value);
+  console.log(amigos);
+  validaNome();
+  exibeLista();
+}
+
+function validaNome(){
+  if(document.getElementById("amigo").value === ""){
+  alert("Por favor, insira um nome.");
+  document.getElementById("amigo").focus();
+}else{
+  document.getElementById("amigo").value="";
+  document.getElementById("amigo").focus();
+}
+}
+
+function excluiVazio (addAmigo) {
+  return addAmigo !== "";
+}
+
+function exibeLista() {
+  for(let i = 0; i < amigos.length; i++) {
+    document.getElementById("listaAmigos").innerHTML = amigos.filter(excluiVazio);
+  }
+}
